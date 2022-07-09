2df0a908e000f7ffcd873b8c27e8aacaa8a0111f
tsw303005 <tsw303005@gmail.com>
2022-07-09 16:08:17 +0800

    push to linux env to test
    author: Ivan

--- src/script.sh
+++ src/script.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
+read -s -p "Enter Password for sudo: " sudoPW
+
 echo "start to set up env \n"
-sudo apt update && sudo apt upgrade
+echo $sudoPW | sudo -S apt update && sudo -S apt upgrade
 
 echo "installing curl now \n"
-sudo apt install curl
-
-echo "installing vim now \n"
-sudo apt install vim
-
-echo "installing git now \n"
-sudo apt install git
-
-echo "installing docker"
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo -S apt install curl
