a5c8288193bccbe3a41ff58afcb1e1c67b6d5cf6
tsw303005 <tsw303005@gmail.com>
2022-07-10 16:46:16 +0800

    update setup.sh

--- src/git/git.sh
+++ src/git/git.sh
@@ -4,11 +4,15 @@ set -e
 
 main() {
     echo "starting to setup .gitconfig"
-    if [ -f $HOME/.gitconfig ]; then
-        echo $sudoPW | sudo -S mv $HOME/.gitconfig $BACKUP_DIR/.gitconfig
-    fi
-    export sudoPW="Puremilk!@#$"
+
+    echo "instlling gitconfig file in $REPO_DIR"
     export REPO_DIR=$HOME/linux-env-setup
+    export BACKUP_DIR=$HOME/.linux-env-backup
+
+    if [ -f $HOME/.gitconfig ]; then
+        mv $HOME/.gitconfig $BACKUP_DIR/.gitconfig
+    fi
+    
     echo $sudoPW | sudo -S cp $REPO_DIR/src/git/.gitconfig /.gitconfig
 
     echo "installing git and config .gitconfig file done~"
