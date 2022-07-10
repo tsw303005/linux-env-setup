5a767248b804f6c373549f03201733b9da578438
tsw303005 <tsw303005@gmail.com>
2022-07-11 00:39:43 +0800

    feat: add docker file to test

--- src/docker/docker.sh
+++ src/docker/docker.sh
@@ -10,6 +10,9 @@ main() {
     echo "starting to install docker-compose..."
     echo sudopW | sudo -S apt-get install -y docker-compose-plugin
     echo "installing docker and docker-compose done~"
+
+    # remove get-docker.sh
+    echo sudoPW | sudo -S rm get-docker.sh
 }
 
 main
